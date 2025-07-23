95b6c2dd387f531ca3d1bd6082f15b5ba050c617
majedabusitta <majed.2002.ais@gmail.com>
2025-07-23 18:51:06 +0300

    css fix

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.St-g3LWH.js","_app/immutable/chunks/NZTpNUN0.js","_app/immutable/chunks/CxZY5a9M.js","_app/immutable/chunks/CUzuRiAr.js","_app/immutable/chunks/5Fo5uJU4.js","_app/immutable/chunks/D9QBiUIG.js","_app/immutable/chunks/Dw0WgoH-.js","_app/immutable/chunks/BIk7rPeI.js"];
+export const imports = ["_app/immutable/nodes/1.DvwmqNfg.js","_app/immutable/chunks/NZTpNUN0.js","_app/immutable/chunks/CxZY5a9M.js","_app/immutable/chunks/CUzuRiAr.js","_app/immutable/chunks/5Fo5uJU4.js","_app/immutable/chunks/BW1eIxi1.js","_app/immutable/chunks/Dw0WgoH-.js","_app/immutable/chunks/BIk7rPeI.js"];
 export const stylesheets = [];
 export const fonts = [];
